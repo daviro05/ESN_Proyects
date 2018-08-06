97d17149b7c196ca5338ba5a877491853521d35e
daviro05 <daviro05@ucm.es>
2018-08-06 21:53:01 +0200

    Cambios en aplicacion para añadir nuevos viajes proximamente

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title>Viajes ESN UCM</title>
+</head>
+
+<body>
+<div class="container">
+    <div>
+        <h1>Viajes ESN UCM 2018/2019</h1>
+        <h2>Próximamente...</h2>
+    </div>
+
+	<div class="bird-container">
+		<div class="bird"></div>
+	</div>
+
+</div>
+</body>
+</html>
