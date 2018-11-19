99a228e163b44b25f3508e14d4721cf53bcd99cd
unknown <daviro05@ucm.es>
2018-11-19 12:03:41 +0100

    cuenta atras viaje

--- GaliciaProyect/portugal/promo/main.js
+++ GaliciaProyect/portugal/promo/main.js
@@ -3,7 +3,7 @@ const countdown = document.querySelector(".countdown");
 const venta = document.querySelector(".venta");
 
 // Set launch date
-const launchDate = new Date("Nov 16, 2018 12:00:00").getTime();
+const launchDate = new Date("Dec 06, 2018 01:00:00").getTime();
 
 const intvl = setInterval(() => {
   const now = new Date().getTime();
@@ -18,14 +18,15 @@ const intvl = setInterval(() => {
   const seconds = Math.floor((daysLeft % (1000 * 60)) / 1000);
 
   countdown.innerHTML = `
-    <div><strong>${hours}</strong><span>Hora</span></div>
+    <div><strong>${daysLeft}</strong><span>Dias</span></div>
+    <div><strong>${hours}</strong><span>Horas</span></div>
     <div><strong>${minutes}</strong><span>Minutos</span></div>
     <div><strong>${seconds}</strong><span>Segundos</span></div>
   `;
 
   if(hours<0 && minutes <0 && seconds<0){
     clearInterval(intvl)
-    countdown.innerHTML = `¡PRONTO NUEVOS TICKETS A LA VENTA!`;
+    countdown.innerHTML = `¡Nos vamos de viaje!`;
     //venta.innerHTML=`<a href='https://verse.events/QYT3ojb6wuets'>Enlace a Verse</a>`;
     venta.innerHTML=`ESN UCM`;
   }
